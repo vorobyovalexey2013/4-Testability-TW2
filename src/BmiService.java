b9bbb55d24b9d852208ff36fb63111e2e629f1cd
Alexey <valeskey1988@mail.ru>
2023-02-13 23:04:07 +0500

    first commit

---
+++ src/BmiService.java
@@ -0,0 +1,9 @@
+public class BmiService {
+    public double calculate(double height, double weight) {
+       double ind = (weight / (height * height));
+       return ind;
+
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {System.out.println("Hello world!");
+        BmiService service = new BmiService();
+        double height = 1.87; // метров
+        double weight = 98; // килограмм
+        double Bmi = (int) service.calculate(height, weight);
+        System.out.println("Индекс Вашего тела: " + Bmi);
+
+
+    }
+}
